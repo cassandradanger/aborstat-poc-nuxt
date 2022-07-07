25e1fe70c7fe49f9070eb6673d5cda589d455d85
casie siekman <casie@soona.co>
2022-07-06 23:46:16 -0500

    i have no idea what i'm doing

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const info = require('./api/info.js')
 const app = express();
+const port = process.env.port || 9000;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://main--bespoke-pixie-dd6ecd.netlify.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -17,6 +17,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/info', info);
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.listen(9000, () => {
-  console.log('Running on port 9000')
+app.listen(port, () => {
+  console.log(`Running on port ${port}`)
 });
